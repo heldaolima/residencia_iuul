1bd21094132c776e50cbcef7928c58759d612107
Hélder Lima <orfeowrk@gmail.com>
2024-10-25 11:06:06 -0300

    desafio 1.1: Piramide

---
+++ desafio_1_1/Piramide.Tests/Piramide_Should.cs
@@ -0,0 +1,28 @@
+namespace Piramide.Tests;
+using DesafiosCSharp.Piramide;
+
+public class Piramide_Should
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Piramide_ShouldThrow_IfNIsLowerThan1(int value)
+    {
+        Assert.Throws<ArgumentException>(() => new Piramide(value));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Piramide_ShouldDraw_IfNIsBiggerThan1(int value)
+    {
+        Piramide piramide = new Piramide(value);
+        Console.WriteLine($"Desenha {value}:");
+        piramide.Desenha();
+        Console.WriteLine();
+        Assert.True(true);
+    }
+}
---
+++ desafio_1_1/Piramide/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace DesafiosCSharp.Piramide;
+public class Piramide
+{
+  public int N;
+  public Piramide(int _N)
+  {
+    if (_N < 1)
+    {
+      throw new ArgumentException("N deve ser >= 1.");
+    }
+    N = _N;
+  }
+
+  private void DesenhaEspacos(int i)
+  {
+    for (int j = 0; j < N - i; j++)
+    {
+      Console.Write(" ");
+    }
+  }
+
+  public void Desenha()
+  {
+    for (int i = 1; i <= N; i++)
+    {
+      DesenhaEspacos(i);
+
+      for (int j = 1; j <= i; j++)
+      {
+        Console.Write($"{j}");
+      }
+
+      for (int j = i - 1; j >= 1; j--)
+      {
+        Console.Write($"{j}");
+      }
+
+      Console.WriteLine();
+    }
+  }
+}
