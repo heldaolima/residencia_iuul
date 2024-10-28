9384a797c3c6e4c44c2352d19eef6070dc75040d
Hélder Lima <orfeowrk@gmail.com>
2024-10-28 15:25:02 -0300

    projeto ValidacaoDados

--- desafio_1_1/Armstrong/Main.cs
+++ desafio_1_1/Armstrong/Main.cs
@@ -1,11 +1,9 @@
 using DesafiosCSharp.Projeto_7;
 
-class Program {
-  static void Main(string[] args) {
-    for (int i = 1; i <= 10000; i++) {
-      if (i.IsArmstrong()) {
-        Console.WriteLine(i);
-      }
-    }
+for (int i = 1; i <= 10000; i++)
+{
+  if (i.IsArmstrong())
+  {
+    Console.WriteLine(i);
   }
 }
